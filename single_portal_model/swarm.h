17f0f56b05bbb49dbffd842ad3adc7b8701ede75
Karan Kumar Budhraja <karank.budhraja@gmail.com>
2013-05-09 15:19:06 +0530

    Update swarm.h
    Update version

--- single_portal_model/swarm.h
+++ single_portal_model/swarm.h
@@ -1,38 +1,40 @@
-/* 
- * \author Karan Kumar Budhraja 
- * (email : karank.budhraja@gmail.com)
- * \author Ashutosh Singh
- * (email : ashutoshs89@gmail.com)
- * \author Gaurav Dubey
- * (email : gaurav311086@gmail.com)
- * \version 0.1
- * \date 2011/08/28
- * \note This software is in the public domain, 
- * furnished "as is", without technical support, and  
- * with no warranty, express or implied, as to its  
- * usefulness for any purpose.                                             
- */
-
 /*******************************************************
- * Swarm.h                                             *
- *******************************************************
- * Model used for a swarm of particles                 *
- *******************************************************/
+* This software is in the public domain, furnished "as *
+* is", without technical support, and with no warranty,*
+* express or implied, as to its usefulness for any       *
+* purpose.											   *
+********************************************************
+* Swarm.h											   *
+********************************************************
+* Model used for a swarm of particles			       *
+*													   *
+* @author Karan Kumar Budhraja						   *
+* @email karank.budhraja@gmail.com					   *
+* @author Ashutosh Singh							   *
+* @email ashutoshs89@gmail.com						   *
+* @author Gaurav Dubey								   *
+* @email gaurav311086@gmail.com						   *
+*													   *
+* @date 28/08/2011									   *
+*													   *
+* @ver 0.1											   *
+*													   *
+*******************************************************/
 
 #ifndef _SWARM_H_
 #define _SWARM_H_
 
-#include "Particle.h"                                   /* A swarm consists of many particles */
+#include "Particle.h"									/* a swarm consists of many particles */
 
-class Swarm                                             /** \brief Definition of a swarm */
+class Swarm 
 {
-    public:
-        Particle Birds[SwarmSize];                      ///< Brief : Swarm of birds 
-        int Best;                                       ///< Brief : Index of global best solution 
+	public:
+		Particle Birds[SwarmSize];						/* swarm of birds */
+		int Best;										/* index of global best solution */
 
-        Swarm();                                        ///< Brief : Constructor to initialize swarm 
-        void Move();                                    ///< Brief : Each particle makes one move 
-        void GetGBestIndex();                           ///< Brief : Read index of global best solution 
+		Swarm();										/* constructor to initialize swarm */
+		void Move();									/* each particle makes one move */
+		void GetGBestIndex();							/* read index of global best solution */
 };
 
 #endif /* _SWARM_H_ */
