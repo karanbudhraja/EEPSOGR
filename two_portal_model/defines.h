1ddff2c5ff81a9f7629acd14df325913a1d1fefc
Karan Kumar Budhraja <karank.budhraja@gmail.com>
2013-05-09 15:22:26 +0530

    Update defines.h
    Update version

--- two_portal_model/defines.h
+++ two_portal_model/defines.h
@@ -1,53 +1,53 @@
 /*******************************************************
 * This software is in the public domain, furnished "as *
 * is", without technical support, and with no warranty,*
-* express or implied, as to its usefulness for any     *
-* purpose.                                             *
+* express or implied, as to its usefulness for any                                                           *
+* purpose.											   *
 ********************************************************
-* Defines.h                                            *
+* Defines.h											   *
 ********************************************************
-* Common definitions used for the model                *
-*                                                      *
-* @author Karan Kumar Budhraja                         *
-* @email karank.budhraja@gmail.com                     *
-* @author Ashutosh Singh                               *
-* @email ashutoshs89@gmail.com                         *
-* @author Gaurav Dubey                                 *
-* @email gaurav311086@gmail.com                        *
-*                                                      *
-* @date 28/08/2011                                     *
-*                                                      *
-* @ver 0.1                                             *
-*                                                      *
+* Common definitions used for the model			       *
+*													   *
+* @author Karan Kumar Budhraja						   *
+* @email karank.budhraja@gmail.com					   *
+* @author Ashutosh Singh							   *
+* @email ashutoshs89@gmail.com						   *
+* @author Gaurav Dubey								   *
+* @email gaurav311086@gmail.com						   *
+*													   *
+* @date 28/08/2011									   *
+*													   *
+* @ver 0.1											   *
+*													   *
 *******************************************************/
 
 #ifndef _DEFINES_H_
 #define _DEFINES_H_
 
-#define DE_JONG             1                           ///< DeJong fitness function
-#define GRIEWANK            2                           ///< Griewank fitness function
-#define RASTRIGIN_F1        3                           ///< Rastrigin fitness function 1
-#define SCHAFFER_F6         4                           ///< Schaffer fitness function 6
-#define ROSENBROCK          5                           ///< Rosenbrock fitness function
-                                                        /* Select function */
-//#define FUNCTION          DE_JONG                     ///< Function to be optimized
-//#define FUNCTION          GRIEWANK                    ///< Function to be optimized
-#define FUNCTION            RASTRIGIN_F1                ///< Function to be optimized
-//#define FUNCTION          SCHAFFER_F6                 ///< Function to be optimized
-//#define FUNCTION          ROSENBROCK                  ///< Function to be optimized
+#define DE_JONG				1							/* enumerate functions */
+#define GRIEWANK			2
+#define RASTRIGIN_F1		3
+#define SCHAFFER_F6			4
+#define ROSENBROCK			5
 
-#define SwarmSize           +25                         ///< Number of particles in swarm
-#define Dimension           +10                         ///< Number of dimensions
-#define LowerLimit          -5.12                       ///< Lowermost position value in a dimension
-#define UpperLimit          +5.12                       ///< Uppermost position value in a dimension 
-#define C1                  +1.8                        ///< Multiplication factor for personal best position pull 
-#define C2                  +1.8                        ///< Multiplication factor for global best position pull 
-#define Weight              +0.6                        ///< Weight of particle 
-//#define Precision         0.00000001                  /* TODO: precision criteria for ending process */
+//#define FUNCTION			DE_JONG						/* select function */
+//#define FUNCTION			GRIEWANK
+//#define FUNCTION			RASTRIGIN_F1
+//#define FUNCTION			SCHAFFER_F6
+#define FUNCTION			ROSENBROCK
 
-#define PORTAL_TELEPORT                                 ///< Enable teleportation
-//#define PORTAL_PULL                                   /* TODO: enable a pull towards the portal */
+#define SwarmSize			+25							/* number of particles in swarm */
+#define Dimension			+10							/* number of dimensions */
+#define LowerLimit			-5.12						/* lowermost position value in a dimension */
+#define UpperLimit			+5.12						/* uppermost position value in a dimension */
+#define C1					+1.8						/* multiplication factor for personal best position pull */
+#define C2					+1.8						/* multiplication factor for global best position pull */
+#define Weight				+.6							/* weight of particle */
+//#define Precision			0.00000001					/* TODO: precision criteria for ending process */
 
-typedef long double  Real ;                             ///< Define a real number
+#define PORTAL_TELEPORT									/* enable teleportation */
+//#define PORTAL_PULL									/* TODO: enable a pull towards the portal */
+
+typedef long double  Real ;								/* define a real number */
 
 #endif /* _DEFINES_H_ */
