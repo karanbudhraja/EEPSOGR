64b1cd953134149c11d50b656de049c99f316e38
Karan Kumar Budhraja <karank.budhraja@gmail.com>
2013-05-09 15:20:07 +0530

    Update engine.cpp
    Update version

--- two_portal_model/engine.cpp
+++ two_portal_model/engine.cpp
@@ -1,145 +1,144 @@
-/** \mainpage Summary
- *
- * This code is the implementation of the two portal 
- * model of PSO. The model has been proposed by the authors
- * as a variant of the basic version of PSO. Guided 
- * re-initialization is used to avoid premature 
- * convergence of particles.
- *
- * \author Karan Kumar Budhraja 
- * (email : karank.budhraja@gmail.com)
- * \author Ashutosh Singh
- * (email : ashutoshs89@gmail.com)
- * \author Gaurav Dubey
- * (email : gaurav311086@gmail.com)
- * \version 0.1
- * \date 2011/08/28
- * \note This software is in the public domain, 
- * furnished "as is", without technical support, and  
- * with no warranty, express or implied, as to its  
- * usefulness for any purpose.                                             
- */
-
 /*******************************************************
- * Engine.cpp                                          *
- *******************************************************
- * Two Portal model of PSO                             *
- *******************************************************/
+* This software is in the public domain, furnished "as *
+* is", without technical support, and with no warranty,*
+* express or implied, as to its usefulness for any                                                           *
+* purpose.											   *
+********************************************************
+* Engine.cpp										   *
+********************************************************
+* Exexution of multiple iterations of algorithm        *
+*													   *
+* @author Karan Kumar Budhraja						   *
+* @email karank.budhraja@gmail.com					   *
+* @author Ashutosh Singh							   *
+* @email ashutoshs89@gmail.com						   *
+* @author Gaurav Dubey								   *
+* @email gaurav311086@gmail.com						   *
+*													   *
+* @date 28/08/2011									   *
+*													   *
+* @ver 0.1											   *
+*													   *
+*******************************************************/
 
 #include<iostream>
 #include<fstream>
 #include<ctime>
 
-#include "Swarm.h"                                      /* definition of a swarm */
+#include "Swarm.h"										/* definition of a swarm */
 
 using namespace std;
 
-#ifdef PORTAL_TELEPORT                                  
-                                                        /* defined in portal.cpp */
-    extern Real OuterRoE;                               ///< RoE of outer portal                         
-    extern Real InnerRoE;                               ///< RoE of inner portal 
-    extern Real InitialOuterRoE;                        ///< Initial RoE of outer portal  
-    extern Real InitialInnerRoE;                        ///< Initial RoE of inner portal  
-    extern Real OuterRoEIncreaseFactor;                 ///< Rate of increase of outer RoE  
-    extern Real InnerRoEDecreaseFactor;                 ///< Rate of decrease of inner RoE  
-    extern int TeleportTypeChangeInterval;              ///< Rate of conversion of particles from outer portal to inner portal teleportation  
+#ifdef PORTAL_TELEPORT
+	extern Real OuterRoE, InnerRoE;						/* defined in portal.cpp */
+	extern Real InitialOuterRoE;					
+	extern Real InitialInnerRoE;					
+	extern Real OuterRoEIncreaseFactor;				
+	extern Real InnerRoEDecreaseFactor;				
+	extern int TeleportTypeChangeInterval;			
 #endif
-    
-/**
- *******************************************************
- * Perform several iterations of pso                    
- *                                                      
- * Input    : Parameters for teleportation (optional)    
- *                                                      
- * Output   : 0 (always)                                         
- *******************************************************
- */
+	
+/*******************************************************
+* main												   *
+********************************************************
+* Perform several iterations of pso					   *
+********************************************************
+*													   *
+* Input						: Parameters (optional)	   *
+*													   *
+********************************************************
+*													   *
+* Output											   *
+*													   *
+*	0 (always)										   *
+*													   *
+*******************************************************/
 int main(int argc, char *argv)
 {
-    const int executions = 10;                          /* number of times the program is executed */
-    Real avg = 0;                                       /* average fitness value */
-    
-    srand((unsigned int)time(NULL));                    /* initialize seed so that we get different results every time */
-    
-    switch(argc)                                        /* if the parameters have been entered */
-    {
-        case 2: 
-        {
-            InitialOuterRoE = argv[1];                      
-            
-            break;
-        }
+	const int executions = 10;							/* number of times the program is executed */
+	Real avg = 0;										/* average fitness value */
+	
+	srand((unsigned int)time(NULL));					/* initialize seed so that we get different results every time */
+	
+	switch(argc)										/* if the parameters have been entered */
+	{
+		case 2:	
+		{
+			InitialOuterRoE = argv[1];						
+			
+			break;
+		}
 
-        case 3:
-        {
-            InitialOuterRoE = argv[1];                      
-            InitialInnerRoE = argv[2];
-            
-            break;
-        }
+		case 3:
+		{
+			InitialOuterRoE = argv[1];						
+			InitialInnerRoE = argv[2];
+			
+			break;
+		}
 
-        case 4:
-        {
-            InitialOuterRoE = argv[1];                      
-            InitialInnerRoE = argv[2];
-            OuterRoEIncreaseFactor = argv[3];
-            
-            break;
-        }
+		case 4:
+		{
+			InitialOuterRoE = argv[1];						
+			InitialInnerRoE = argv[2];
+			OuterRoEIncreaseFactor = argv[3];
+			
+			break;
+		}
 
-        case 5:
-        {
-            InitialOuterRoE = argv[1];                      
-            InitialInnerRoE = argv[2];
-            OuterRoEIncreaseFactor = argv[3];
-            InnerRoEDecreaseFactor = argv[4];
+		case 5:
+		{
+			InitialOuterRoE = argv[1];						
+			InitialInnerRoE = argv[2];
+			OuterRoEIncreaseFactor = argv[3];
+			InnerRoEDecreaseFactor = argv[4];
 
-            break;
-        }
+			break;
+		}
 
-        case 6:
-        {
-            InitialOuterRoE = argv[1];                      
-            InitialInnerRoE = argv[2];
-            OuterRoEIncreaseFactor = argv[3];
-            InnerRoEDecreaseFactor = argv[4];
-            TeleportTypeChangeInterval = argv[5];
+		case 6:
+		{
+			InitialOuterRoE = argv[1];						
+			InitialInnerRoE = argv[2];
+			OuterRoEIncreaseFactor = argv[3];
+			InnerRoEDecreaseFactor = argv[4];
+			TeleportTypeChangeInterval = argv[5];
 
-            break;
-        }
+			break;
+		}
 
-        default: 
-        {
-            break;
-        }
-    }           
-                                                        /* display parameters */
-    cout<<"Parameters: "<<" "<<InitialOuterRoE<<" "<<InitialInnerRoE<<" "<<OuterRoEIncreaseFactor<<" "<<InnerRoEDecreaseFactor<<" "<<TeleportTypeChangeInterval;
+		default: 
+		{
+			break;
+		}
+	}			
+														/* display parameters */
+	cout<<"Parameters: "<<" "<<InitialOuterRoE<<" "<<InitialInnerRoE<<" "<<OuterRoEIncreaseFactor<<" "<<InnerRoEDecreaseFactor<<" "<<TeleportTypeChangeInterval;
 
-    for(int x = 0; x < executions; x++)
-    {
+	for(int x = 0; x < executions; x++)
+	{
             Swarm *S = new Swarm;
-            
+			
             for(int i = 0; i < 1000; i++)
-            {
-                    S->Move();                          /* every particle moves once */
-            }
+			{
+                    S->Move();							/* every particle moves once */
+			}
 
             cout<<"\n\nRun  :  "<<x+1<<"\tFitness : "<<S->Birds[S->Best].GetFitness();
 
 #ifdef PORTAL_TELEPORT
-                                                        /* display run details */
-            cout<<"\tTeleports : "<<GetOuterPortalTeleports()<<" "<<GetInnerPortalTeleports();
-            cout<<"\tRoE : "<<OuterRoE<<" "<<InnerRoE;
+														/* display run details */
+			cout<<"\tTeleports : "<<GetOuterPortalTeleports()<<" "<<GetInnerPortalTeleports();
+			cout<<"\tRoE : "<<OuterRoE<<" "<<InnerRoE;
 #endif
 
             avg+=S->Birds[S->Best].GetFitness();
             delete(S);
     }
-                                                            
-    cout<<"\n\nAverage Fitness = "<<avg/executions;     /* display result */
+														    
+	cout<<"\n\nAverage Fitness = "<<avg/executions;		/* display result */
     
-    cin.get();
+	cin.get();
     
-    return 0;
+	return 0;
 }
